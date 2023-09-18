94f8c59e2ac8f6d0678774c15365bbf098ff0d04
Max Erik Rodriguez <86026606+ProBats@users.noreply.github.com>
2023-09-18 17:54:10 -0300

    Add files via upload

---
+++ Clase05/Clase05/src/main/java/ar/org/centro8/curso/especialidad/aplicacione/web/Clase04Parte2JSP/Clase04Parte2JspApplication.java
@@ -0,0 +1,13 @@
+package ar.org.centro8.curso.especialidad.aplicacione.web.Clase04Parte2JSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase04Parte2JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clase04Parte2JspApplication.class, args);
+	}
+
+}
---
+++ Clase05/Clase05/src/test/java/ar/org/centro8/curso/especialidad/aplicacione/web/Clase04Parte2JSP/Clase04Parte2JspApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.org.centro8.curso.especialidad.aplicacione.web.Clase04Parte2JSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase04Parte2JspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
